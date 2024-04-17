dc3cdc5b20496dc50280b304d303b0c64b645af0
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-17 19:31:14 +0100

    refactor: Use 'rand' create to generate a random number

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess a number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
     println!("Please input your guess");
 
