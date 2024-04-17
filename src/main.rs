9fd4e1d46edc9bd0063a6046a964a880e06ff5c0
Tirso Samalungo <136708640+GreyTearsDev@users.noreply.github.com>
2024-04-17 19:53:07 +0100

    refactor: add logic for comparing the user guess to the randomly generated number

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -15,4 +16,12 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guessed: {}", guess);
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
